ec63aa62da3a5438fc49d3af3d95f0926f26c83f
Gordion <45543051+Gordion@users.noreply.github.com>
2019-02-14 00:40:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+
+  init();
+
+  function init() {
+    console.log('init');
+    var isLoggedIn = !!localStorage.getItem('user');
+    if (isLoggedIn) {
+      var user = JSON.parse(localStorage.getItem('user'));
+      console.log(user);
+      $('body').addClass("logged-in");
+      $('.sp-us').text(user.login);
+
+    } else {
+      $('.sp-us').text('Log in');
+
+    }
+  }
+
+  $(".quit-link").on('click', function(e){
+    var isLoggedIn = !!localStorage.getItem('user');
+    if (isLoggedIn) {
+      localStorage.removeItem('user');
+      $('.sp-us').text('Log in');
+    }
+  });
+
+  $("#reg-button").on('click', function(e) {
+    e.preventDefault();
+
+    var regData = $('#reg-form').serialize();
+    console.log(regData);
+
+    $.post( "http://transleet.somee.com/translationHelper/WebHandler.ashx?method=Register", regData)
+      .done(function(data) {
+        var response = JSON.parse(data);
+        console.log(data, response);
+
+        if (response.IsValid === "false") {
+          alert(response.ERROR);
+        }
+        else {
+          window.open("log in.html");
+          // alert("Registred");
+        }
+      });
+
+    return false;
+  });
+
+      // $.ajax({
+      //   url: "http://transleet.somee.com/httpHndlr.ashx",
+      //   type: "get",
+      //   contentType: "application/json; charset=utf-8",
+      //   dataType: "json",
+      // });
+
+    // var dataReg = JSON.stringify( $(this).serializeArray() );
+    // console.log(dataReg);
+
+    // $.get("http://transleet.somee.com/servicehelper/Register.ashx", function(data) {
+    //   var user = JSON.parse(data);
+    //
+    //   console.log(user.name);
+    //   console.log(user.surname);
+    //
+    //   $('.username').text(user.name + ' ' + user.surname);
+    //
+    //   // alert("Name: " + JSON.stringify(name) + "\n Surname: " + JSON.stringify(surname));
+    //   // console.log(name)
+    // })
+
+
+
+  $("#log-button").on('click', function(e) {
+    e.preventDefault();
+
+    var regData = $('#log-form').serialize();
+
+    $.post( "http://transleet.somee.com/translationHelper/WebHandler.ashx?method=SignIn", regData)
+      .done(function(data) {
+        var response = JSON.parse(data);
+        console.log(data, response);
+
+        if (response.IsValid === "false") {
+          alert(response.ERROR);
+        }
+        else if (response.IsValid === "true") {
+          var user = {
+            login: $('.input-login').val(),
+            guid: response.UserGuid
+          };
+          localStorage.setItem('user', JSON.stringify(user));
+          window.location ='vocabulary.html';
+        }
+      });
+
+    return false;
+  });
+
+});
